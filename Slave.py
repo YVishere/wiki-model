09c2f2e68339fbb96390194ff09b9ff1e83cef00
Aditya YV <adityayv802@gmail.com>
2024-11-06 16:04:54 -0600

    Improved main

--- Slave.py
+++ Slave.py
@@ -7,12 +7,6 @@ def main(uarr):
     with mp.Pool(processes=mp.cpu_count()) as pool:
         results = [pool.apply_async(Scraper.startScraping, args=(uarr[i],' ',True)) for i in range(len(uarr))]
         
-        for ind, result in enumerate(results):
-            print("Started process ", ind)
-        
         output = [result.get() for result in results]
-        
-        for ind in range(len(results)):
-            print("Joining process ", ind)
 
         return output
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import Scraper
+import Slave
 import numpy as np
 import matplotlib.pyplot as plt
-import Slave
+import time
+
     
-def scatter_plot(X, y):
+def scatter_plot(X, y, blockProgram = False):
+    plt.figure()
     plt.scatter(X, y)
-    plt.show()
-    plt.close()
+    plt.show(block = blockProgram)
     
-def plot_graph(X, y):
+def plot_graph(X, y, blockProgram = False):
     plt.plot(X, y)
-    plt.show()
+    plt.show(block = blockProgram)
     plt.close()
 
 def new_weights(w, ind,k):
@@ -22,21 +24,28 @@ if __name__ == '__main__':
     subject = "Wikipedia"
     result = "Paris"
 
+    #Scraping the data
+    start = time.time()
     uarr = Scraper.startScraping(subject, result)
-
-    print(len(uarr))
+    end = time.time()
+    print("Time taken to scrape",subject,": ", round(end-start, 3),"seconds")
     
-    basew = np.array([1])
-    weights = np.array([])
-    weights = np.append(weights, basew)
+    basewX = np.array([1])
+    wX = np.array([])
+    wX = np.append(wX, basewX)
 
+    #Processing weights
     for i in range(1, len(uarr)):
-        nw = new_weights(basew, i, 5)
-        weights = np.append(weights, nw)
-    
-    #scatter_plot(range(0, len(weights)), weights)
+        nw = new_weights(basewX, i, 5)
+        wX = np.append(wX, nw)
 
+    start = time.time()
+    #Extracting number of links from each page
     output = Slave.main(uarr)
-    
-    print(output)
-    scatter_plot(output, weights)
+    end = time.time()
+    print("Time taken: ", round(end-start, 3),"seconds")
+
+    #Plot data
+    scatter_plot(range(0, len(wX)), wX)
+    scatter_plot(range(0,len(uarr)), output)
+    scatter_plot(output, wX, True)
